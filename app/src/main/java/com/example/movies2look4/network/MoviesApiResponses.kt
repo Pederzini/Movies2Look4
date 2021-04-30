c3ea41a3c59d037d00f31fa84091108c935ecd6d
Pederzini <victor.pederzini@bandtec.com.br>
2021-04-30 14:31:57 -0300

    Adiciona regex para tratar ids

--- app/src/main/java/com/example/movies2look4/network/MoviesApiResponses.kt
+++ app/src/main/java/com/example/movies2look4/network/MoviesApiResponses.kt
@@ -2,12 +2,14 @@ package com.example.movies2look4.network
 
 import android.util.Log
 import com.example.movies2look4.network.MoviesApiConnection.moviesApiService
+import org.intellij.lang.annotations.RegExp
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MoviesApiResponses {
 
+    val moviesIdOnly = mutableListOf<String>()
     fun getMoviesIds() {
 
         moviesApiService().getMoviesIds().enqueue(object : Callback<List<String>> {
@@ -16,8 +18,13 @@ class MoviesApiResponses {
             }
 
             override fun onResponse(call: Call<List<String>>, response: Response<List<String>>) {
-                Log.e("AAAAAAAAAAAAAAAAAAAAA", response.body().toString())
+                val pattern = "title|/".toRegex()
 
+                response.body()?.forEach {
+                    moviesIdOnly.add(it.replace(pattern, ""))
+                }
+
+                println(moviesIdOnly.toString())
             }
         })
 
