c8a9c375ad9812a76402b15fa5d8ef1dc1f74980
Pederzini <victor.pederzini@bandtec.com.br>
2021-05-03 15:37:04 -0300

    Adiciona tela de detalhes do filme

---
+++ app/src/main/java/com/example/movies2look4/ui/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movies2look4.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movies2look4.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
